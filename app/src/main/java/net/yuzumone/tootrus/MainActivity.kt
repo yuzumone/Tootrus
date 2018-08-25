15360c7cd8ebdab056644f69a15c22e97aabac6a
yuzumone <yuzumone@outlook.com>
2018-08-25 09:36:21 +0900

    Change theme to NoActionBar
    close #1

--- app/src/main/java/net/yuzumone/tootrus/MainActivity.kt
+++ app/src/main/java/net/yuzumone/tootrus/MainActivity.kt
@@ -1,12 +1,17 @@
 package net.yuzumone.tootrus
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import net.yuzumone.tootrus.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setSupportActionBar(binding.toolbar)
     }
 }
