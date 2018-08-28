a67a9fe9a38d9e6b800ca67ddbd9f5d53d9e92a7
yuzumone <yuzumone@outlook.com>
2018-08-28 21:11:16 +0900

    Add oauth repository

--- app/src/main/java/net/yuzumone/tootrus/di/ApplicationModule.kt
+++ app/src/main/java/net/yuzumone/tootrus/di/ApplicationModule.kt
@@ -1,11 +1,15 @@
 package net.yuzumone.tootrus.di
 
 import android.content.Context
+import com.google.gson.Gson
 import dagger.Module
 import dagger.Provides
 import net.yuzumone.tootrus.App
+import net.yuzumone.tootrus.data.mastodon.DefaultOAuthRepository
+import net.yuzumone.tootrus.data.mastodon.OAuthRepository
 import net.yuzumone.tootrus.data.prefs.PreferenceStorage
 import net.yuzumone.tootrus.data.prefs.SharedPreferenceStorage
+import okhttp3.OkHttpClient
 import javax.inject.Singleton
 
 @Module
@@ -18,6 +22,21 @@ class ApplicationModule {
 
     @Singleton
     @Provides
+    fun providesOkHttpClientBuilder(): OkHttpClient.Builder =
+            OkHttpClient.Builder()
+
+    @Singleton
+    @Provides
+    fun providesGson(): Gson =
+            Gson()
+
+    @Singleton
+    @Provides
     fun providesPreferenceStorage(context: Context): PreferenceStorage =
             SharedPreferenceStorage(context)
+
+    @Singleton
+    @Provides
+    fun providesOAuthRepository(repository: DefaultOAuthRepository): OAuthRepository =
+            repository
 }
