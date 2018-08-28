f43adbcfb0f7be848b8490624fb42faa5722c166
yuzumone <yuzumone@outlook.com>
2018-08-29 07:52:53 +0900

    Add main viewmodel

--- app/src/main/java/net/yuzumone/tootrus/di/ActivityModule.kt
+++ app/src/main/java/net/yuzumone/tootrus/di/ActivityModule.kt
@@ -1,10 +1,13 @@
 package net.yuzumone.tootrus.di
 
+import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
 import dagger.Binds
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
+import dagger.multibindings.IntoMap
 import net.yuzumone.tootrus.ui.MainActivity
+import net.yuzumone.tootrus.ui.MainViewModel
 
 @Module
 abstract class ActivityModule {
@@ -14,4 +17,9 @@ abstract class ActivityModule {
 
     @ContributesAndroidInjector(modules = [MainActivityModule::class])
     abstract fun contributeMainActivity(): MainActivity
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(MainViewModel::class)
+    abstract fun bindSettingViewModel(viewModel: MainViewModel): ViewModel
 }
--- app/src/main/java/net/yuzumone/tootrus/ui/MainActivity.kt
+++ app/src/main/java/net/yuzumone/tootrus/ui/MainActivity.kt
@@ -1,9 +1,13 @@
 package net.yuzumone.tootrus.ui
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import dagger.android.AndroidInjection
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
@@ -16,16 +20,29 @@ import javax.inject.Inject
 class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mainViewModel: MainViewModel
     @Inject lateinit var dispatchingActivityInjector: DispatchingAndroidInjector<Fragment>
+    @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setSupportActionBar(binding.toolbar)
+        mainViewModel = ViewModelProviders.of(this, viewModelFactory)
+                .get(MainViewModel::class.java)
         if (savedInstanceState == null) {
-            val fragment = OAuthFragment()
-            supportFragmentManager.beginTransaction().add(R.id.content, fragment).commit()
+            mainViewModel.setFragment.observe(this, Observer {
+                when (it) {
+                    SetFragment.OAUTH -> {
+                        supportFragmentManager.beginTransaction()
+                                .add(R.id.content, OAuthFragment()).commit()
+                    }
+                    SetFragment.TOP -> {
+                        Log.d("MainActivity", "Top fragment")
+                    }
+                }
+            })
         }
     }
 
