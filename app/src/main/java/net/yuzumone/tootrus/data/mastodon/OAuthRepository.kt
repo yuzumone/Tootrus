f6395f0c704341be08bd1bca79b0ae08d46c2902
yuzumone <yuzumone@outlook.com>
2018-08-28 20:46:47 +0900

    Add mastodon oauth
    close #7

---
+++ app/src/main/java/net/yuzumone/tootrus/data/mastodon/OAuthRepository.kt
@@ -0,0 +1,47 @@
+package net.yuzumone.tootrus.data.mastodon
+
+import com.google.gson.Gson
+import com.sys1yagi.mastodon4j.MastodonClient
+import com.sys1yagi.mastodon4j.api.Scope
+import com.sys1yagi.mastodon4j.api.entity.auth.AccessToken
+import com.sys1yagi.mastodon4j.api.method.Apps
+import net.yuzumone.tootrus.vo.OAuthParameter
+import okhttp3.OkHttpClient
+import javax.inject.Inject
+
+interface OAuthRepository {
+    fun getOAuthParameter(instanceName: String): OAuthParameter
+    fun getAccessToken(instanceName: String, clientId: String, clientSecret: String, code:String):
+            AccessToken
+}
+
+class DefaultOAuthRepository @Inject constructor(
+        private val okHttpClientBuilder: OkHttpClient.Builder,
+        private val gson: Gson
+) : OAuthRepository {
+
+    override fun getOAuthParameter(instanceName: String): OAuthParameter {
+        val client = MastodonClient.Builder(instanceName, okHttpClientBuilder, gson).build()
+        val apps = Apps(client)
+        val registration = apps.createApp(
+                clientName = "Tootrus",
+                redirectUris = "urn:ietf:wg:oauth:2.0:oob",
+                scope = Scope(Scope.Name.ALL),
+                website = "https://github.com/yuzumone/Tootrus"
+        ).execute()
+        val url = apps.getOAuthUrl(registration.clientId, Scope(Scope.Name.ALL))
+        return OAuthParameter(instanceName, registration.clientId, registration.clientSecret, url)
+    }
+
+    override fun getAccessToken(instanceName: String, clientId: String, clientSecret: String,
+                                code: String): AccessToken {
+        val client = MastodonClient.Builder(instanceName, okHttpClientBuilder, gson).build()
+        val apps = Apps(client)
+        return apps.getAccessToken(
+                clientId = clientId,
+                clientSecret = clientSecret,
+                code = code,
+                grantType = "authorization_code"
+                ).execute()
+    }
+}
--- app/src/main/java/net/yuzumone/tootrus/ui/MainActivity.kt
+++ app/src/main/java/net/yuzumone/tootrus/ui/MainActivity.kt
@@ -1,18 +1,52 @@
 package net.yuzumone.tootrus.ui
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import dagger.android.AndroidInjection
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.support.HasSupportFragmentInjector
 import net.yuzumone.tootrus.R
 import net.yuzumone.tootrus.databinding.ActivityMainBinding
+import net.yuzumone.tootrus.ui.oauth.OAuthFragment
+import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mainViewModel: MainViewModel
+    @Inject lateinit var dispatchingActivityInjector: DispatchingAndroidInjector<Fragment>
+    @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setSupportActionBar(binding.toolbar)
+        mainViewModel = ViewModelProviders.of(this, viewModelFactory)
+                .get(MainViewModel::class.java)
+        if (savedInstanceState == null) {
+            mainViewModel.setFragment.observe(this, Observer {
+                when (it) {
+                    SetFragment.OAUTH -> {
+                        supportFragmentManager.beginTransaction()
+                                .add(R.id.content, OAuthFragment()).commit()
+                    }
+                    SetFragment.TOP -> {
+                        Log.d("MainActivity", "Top fragment")
+                    }
+                }
+            })
+        }
+    }
+
+    override fun supportFragmentInjector(): AndroidInjector<Fragment> {
+        return dispatchingActivityInjector
     }
 }
