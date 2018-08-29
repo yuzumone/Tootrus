ba3add208d1ab0a8c94e02f9f8a121c7124bb8a2
yuzumone <yuzumone@outlook.com>
2018-08-30 06:50:37 +0900

    Make accessing property using UseCase
    close #8

--- app/src/main/java/net/yuzumone/tootrus/ui/MainViewModel.kt
+++ app/src/main/java/net/yuzumone/tootrus/ui/MainViewModel.kt
@@ -3,19 +3,24 @@ package net.yuzumone.tootrus.ui
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
-import net.yuzumone.tootrus.data.prefs.PreferenceStorage
+import net.yuzumone.tootrus.domain.Success
+import net.yuzumone.tootrus.domain.prefs.GetAccessTokenPrefUseCase
 import net.yuzumone.tootrus.util.map
 import javax.inject.Inject
 
 class MainViewModel @Inject constructor(
-        preferenceStorage: PreferenceStorage
+        getAccessTokenUseCase: GetAccessTokenPrefUseCase
 ) : ViewModel() {
 
     private val accessToken = MutableLiveData<String>()
     val setFragment: LiveData<SetFragment>
 
     init {
-        accessToken.value = preferenceStorage.accessToken
+        getAccessTokenUseCase(Unit) {
+            when (it) {
+                is Success -> accessToken.value = it.value
+            }
+        }
         setFragment = accessToken.map {
             if (it == null) {
                 SetFragment.OAUTH
