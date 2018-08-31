d3f4f0f9291702767643380826fd9035f6c50cf2
yuzumone <yuzumone@outlook.com>
2018-08-31 21:55:24 +0900

    Update timeline repository

--- app/src/main/java/net/yuzumone/tootrus/data/mastodon/TimelineRepository.kt
+++ app/src/main/java/net/yuzumone/tootrus/data/mastodon/TimelineRepository.kt
@@ -1,24 +1,19 @@
 package net.yuzumone.tootrus.data.mastodon
 
-import com.google.gson.Gson
 import com.sys1yagi.mastodon4j.MastodonClient
+import com.sys1yagi.mastodon4j.api.Range
 import com.sys1yagi.mastodon4j.api.entity.Status
 import com.sys1yagi.mastodon4j.api.method.Timelines
-import okhttp3.OkHttpClient
 import javax.inject.Inject
 
 interface TimelineRepository {
-    fun getTimeline(instanceName: String, token: String): List<Status>
+    fun getTimeline(range: Range): List<Status>
 }
 
 class DefaultTimelineRepository @Inject constructor(
-        private val okHttpClientBuilder: OkHttpClient.Builder,
-        private val gson: Gson
+        private val client: MastodonClient
 ): TimelineRepository {
-    override fun getTimeline(instanceName: String, token: String): List<Status> {
-        val client = MastodonClient
-                .Builder(instanceName, okHttpClientBuilder, gson)
-                .accessToken(token).build()
+    override fun getTimeline(range: Range): List<Status> {
         val result = Timelines(client).getHome().execute()
         return result.part
     }
