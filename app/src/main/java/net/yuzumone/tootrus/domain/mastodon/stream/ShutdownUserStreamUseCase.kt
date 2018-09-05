a638786260dfd76150adfcfaeb48c98d8a21d22b
yuzumone <yuzumone@outlook.com>
2018-09-05 20:38:35 +0900

    Add stream usecase

---
+++ app/src/main/java/net/yuzumone/tootrus/domain/mastodon/stream/ShutdownUserStreamUseCase.kt
@@ -0,0 +1,11 @@
+package net.yuzumone.tootrus.domain.mastodon.stream
+
+import net.yuzumone.tootrus.data.mastodon.StreamRepository
+import net.yuzumone.tootrus.domain.UseCase
+import javax.inject.Inject
+
+class ShutdownUserStreamUseCase @Inject constructor(
+        private val repository: StreamRepository
+) : UseCase<Unit, Unit>() {
+    override suspend fun run(params: Unit) = repository.shutdown()
+}
---
+++ app/src/main/java/net/yuzumone/tootrus/domain/mastodon/stream/StartUserStreamUseCase.kt
@@ -0,0 +1,12 @@
+package net.yuzumone.tootrus.domain.mastodon.stream
+
+import com.sys1yagi.mastodon4j.api.Handler
+import net.yuzumone.tootrus.data.mastodon.StreamRepository
+import net.yuzumone.tootrus.domain.UseCase
+import javax.inject.Inject
+
+class StartUserStreamUseCase @Inject constructor(
+        private val repository: StreamRepository
+) : UseCase<Handler, Unit>() {
+    override suspend fun run(params: Handler) = repository.user(params)
+}
