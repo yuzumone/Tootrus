68c469075f75e6434baa9a34562c8a505a668e56
yuzumone <yuzumone@outlook.com>
2018-09-05 20:38:55 +0900

    Add extension for to insert item to list of mutable livedata

--- app/src/main/java/net/yuzumone/tootrus/util/Extensions.kt
+++ app/src/main/java/net/yuzumone/tootrus/util/Extensions.kt
@@ -1,8 +1,23 @@
 package net.yuzumone.tootrus.util
 
 import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.Transformations
 
 fun <X, Y> LiveData<X>.map(body: (X) -> Y): LiveData<Y> {
     return Transformations.map(this, body)
+}
+
+fun <T> MutableLiveData<List<T>>.insertValue(value: T) {
+    val list = arrayListOf<T>()
+    this.value?.forEach { list.add(it) }
+    list.add(0, value)
+    this.value = list
+}
+
+fun <T> MutableLiveData<List<T>>.postInsertValue(value: T) {
+    val list = arrayListOf<T>()
+    this.value?.forEach { list.add(it) }
+    list.add(0, value)
+    this.postValue(list)
 }
