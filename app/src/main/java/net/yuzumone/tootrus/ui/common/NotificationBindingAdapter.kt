c81b641b965a861d347e46cc815228e436de7ea3
yuzumone <yuzumone@outlook.com>
2018-09-07 07:20:28 +0900

    Add notification binding adapter

---
+++ app/src/main/java/net/yuzumone/tootrus/ui/common/NotificationBindingAdapter.kt
@@ -0,0 +1,18 @@
+package net.yuzumone.tootrus.ui.common
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.sys1yagi.mastodon4j.api.entity.Notification
+import net.yuzumone.tootrus.databinding.ItemNotificationBinding
+
+class NotificationBindingAdapter : BindingRecyclerAdapter<Notification, ItemNotificationBinding>() {
+    override fun createBinding(parent: ViewGroup): ItemNotificationBinding {
+        val binding = ItemNotificationBinding
+                .inflate(LayoutInflater.from(parent.context), parent, false)
+        return binding
+    }
+
+    override fun bind(binding: ItemNotificationBinding, item: Notification) {
+        binding.notification = item
+    }
+}
