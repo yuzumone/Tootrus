bd1c92a8c0c46e52522bb26b7def4abd26956acf
yuzumone <yuzumone@outlook.com>
2018-09-09 13:40:53 +0900

    Add local timeline fragment

---
+++ app/src/main/java/net/yuzumone/tootrus/ui/top/local/LocalTimelineFragment.kt
@@ -0,0 +1,55 @@
+package net.yuzumone.tootrus.ui.top.local
+
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dagger.android.support.AndroidSupportInjection
+import net.yuzumone.tootrus.R
+import net.yuzumone.tootrus.databinding.FragmentLocalTimelineBinding
+import net.yuzumone.tootrus.ui.common.StatusBindingAdapter
+import javax.inject.Inject
+
+class LocalTimelineFragment : Fragment() {
+
+    private lateinit var binding: FragmentLocalTimelineBinding
+    private lateinit var adapter: StatusBindingAdapter
+    private lateinit var localTimelineViewModel: LocalTimelineViewModel
+    @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    override fun onAttach(context: Context?) {
+        AndroidSupportInjection.inject(this)
+        super.onAttach(context)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        localTimelineViewModel = ViewModelProviders.of(this, viewModelFactory)
+                .get(LocalTimelineViewModel::class.java)
+        adapter = StatusBindingAdapter()
+        binding = FragmentLocalTimelineBinding.inflate(inflater, container, false).apply {
+            recyclerLocalTimeline.adapter = adapter
+            recyclerLocalTimeline.layoutManager = LinearLayoutManager(activity)
+            swipeRefresh.setColorSchemeResources(R.color.colorPrimary)
+            swipeRefresh.setOnRefreshListener { localTimelineViewModel.updateLocalTimeline() }
+        }
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        localTimelineViewModel.statuses.observe(this, Observer {
+            binding.swipeRefresh.isRefreshing = false
+            adapter.update(it)
+        })
+        localTimelineViewModel.error.observe(this, Observer {
+            binding.swipeRefresh.isRefreshing = false
+        })
+    }
+}
