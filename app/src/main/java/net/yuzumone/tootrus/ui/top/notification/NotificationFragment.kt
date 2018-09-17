f9d92760319bfa6cbc962065c4d60e7b624fbb3b
yuzumone <yuzumone@outlook.com>
2018-09-18 07:18:15 +0900

    Add divider
    close #21

--- app/src/main/java/net/yuzumone/tootrus/ui/top/notification/NotificationFragment.kt
+++ app/src/main/java/net/yuzumone/tootrus/ui/top/notification/NotificationFragment.kt
@@ -6,6 +6,8 @@ import android.arch.lifecycle.ViewModelProviders
 import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
+import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
@@ -35,9 +37,13 @@ class NotificationFragment : Fragment() {
         topViewModel = ViewModelProviders.of(activity!!, viewModelFactory)
                 .get(TopViewModel::class.java)
         adapter = NotificationBindingAdapter()
+        val layoutManager = LinearLayoutManager(activity)
+        val divider = DividerItemDecoration(activity, layoutManager.orientation)
+        divider.setDrawable(ContextCompat.getDrawable(activity!!, R.drawable.divider)!!)
         binding = FragmentNotificationBinding.inflate(inflater, container, false).apply {
             recyclerNotification.adapter = adapter
-            recyclerNotification.layoutManager = LinearLayoutManager(activity)
+            recyclerNotification.layoutManager = layoutManager
+            recyclerNotification.addItemDecoration(divider)
         }
         return binding.root
     }
