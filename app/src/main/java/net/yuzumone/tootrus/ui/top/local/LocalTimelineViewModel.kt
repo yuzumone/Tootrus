cc494e9585618bc22e71de6c6ea3970148cb1e71
yuzumone <yuzumone@outlook.com>
2018-09-29 17:13:54 +0900

    Update ui

--- app/src/main/java/net/yuzumone/tootrus/ui/top/local/LocalTimelineViewModel.kt
+++ app/src/main/java/net/yuzumone/tootrus/ui/top/local/LocalTimelineViewModel.kt
@@ -3,12 +3,12 @@ package net.yuzumone.tootrus.ui.top.local
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import com.sys1yagi.mastodon4j.api.Range
-import com.sys1yagi.mastodon4j.api.entity.Status
 import net.yuzumone.tootrus.domain.Failure
 import net.yuzumone.tootrus.domain.Success
 import net.yuzumone.tootrus.domain.mastodon.status.PostFavoriteUseCase
 import net.yuzumone.tootrus.domain.mastodon.timeline.GetLocalPublicUseCase
 import net.yuzumone.tootrus.util.insertValues
+import net.yuzumone.tootrus.vo.TootrusStatus
 import javax.inject.Inject
 
 class LocalTimelineViewModel @Inject constructor(
@@ -16,8 +16,8 @@ class LocalTimelineViewModel @Inject constructor(
         private val postFavoriteUseCase: PostFavoriteUseCase
 ) : ViewModel() {
 
-    val statuses = MutableLiveData<List<Status>>()
-    val favoritedStatus = MutableLiveData<Status>()
+    val statuses = MutableLiveData<List<TootrusStatus>>()
+    val favoritedStatus = MutableLiveData<TootrusStatus>()
     val error = MutableLiveData<Exception>()
 
     init {
@@ -44,7 +44,7 @@ class LocalTimelineViewModel @Inject constructor(
         }
     }
 
-    fun postFavorite(target: Status) {
+    fun postFavorite(target: TootrusStatus) {
         postFavoriteUseCase(target.id) {
             when (it) {
                 is Success -> favoritedStatus.value = it.value
