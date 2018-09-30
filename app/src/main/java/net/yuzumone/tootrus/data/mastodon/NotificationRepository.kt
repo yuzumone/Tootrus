56598bc30959f6ada0ad1c943618487a7f8a5393
yuzumone <yuzumone@outlook.com>
2018-09-30 12:47:01 +0900

    Update repository

--- app/src/main/java/net/yuzumone/tootrus/data/mastodon/NotificationRepository.kt
+++ app/src/main/java/net/yuzumone/tootrus/data/mastodon/NotificationRepository.kt
@@ -2,20 +2,20 @@ package net.yuzumone.tootrus.data.mastodon
 
 import com.sys1yagi.mastodon4j.MastodonClient
 import com.sys1yagi.mastodon4j.api.Range
-import com.sys1yagi.mastodon4j.api.entity.Notification
 import com.sys1yagi.mastodon4j.api.method.Notifications
+import net.yuzumone.tootrus.vo.TootrusNotification
 import javax.inject.Inject
 import javax.inject.Named
 
 interface NotificationRepository {
-    fun getNotifications(range: Range): List<Notification>
+    fun getNotifications(range: Range): List<TootrusNotification>
 }
 
 class DefaultNotificationRepository @Inject constructor(
         @Named("client") private val client: MastodonClient
 ) : NotificationRepository {
-    override fun getNotifications(range: Range): List<Notification> {
+    override fun getNotifications(range: Range): List<TootrusNotification> {
         val result = Notifications(client).getNotifications(range).execute()
-        return result.part
+        return result.part.map { TootrusNotification(it) }
     }
 }
