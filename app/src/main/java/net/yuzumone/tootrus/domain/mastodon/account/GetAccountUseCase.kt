47b994ebb942c45c1457c5d02c58552d302668c4
yuzumone <yuzumone@outlook.com>
2018-10-24 23:57:00 +0900

    Add get account use case
    close #50

---
+++ app/src/main/java/net/yuzumone/tootrus/domain/mastodon/account/GetAccountUseCase.kt
@@ -0,0 +1,12 @@
+package net.yuzumone.tootrus.domain.mastodon.account
+
+import com.sys1yagi.mastodon4j.api.entity.Account
+import net.yuzumone.tootrus.data.mastodon.AccountRepository
+import net.yuzumone.tootrus.domain.UseCase
+import javax.inject.Inject
+
+class GetAccountUseCase @Inject constructor(
+        private val repository: AccountRepository
+) : UseCase<Long, Account>() {
+    override suspend fun run(params: Long) = repository.getAccount(params)
+}
