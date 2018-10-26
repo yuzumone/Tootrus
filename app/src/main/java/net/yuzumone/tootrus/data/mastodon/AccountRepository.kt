f8680e1000e915a1f1a92ae3ac9272ca789b9cd0
yuzumone <yuzumone@outlook.com>
2018-10-26 23:41:13 +0900

    Update AccountRepository

--- app/src/main/java/net/yuzumone/tootrus/data/mastodon/AccountRepository.kt
+++ app/src/main/java/net/yuzumone/tootrus/data/mastodon/AccountRepository.kt
@@ -2,12 +2,14 @@ package net.yuzumone.tootrus.data.mastodon
 
 import com.sys1yagi.mastodon4j.MastodonClient
 import com.sys1yagi.mastodon4j.api.entity.Account
+import com.sys1yagi.mastodon4j.api.entity.Relationship
 import com.sys1yagi.mastodon4j.api.method.Accounts
 import javax.inject.Inject
 import javax.inject.Named
 
 interface AccountRepository {
     fun getAccount(id: Long): Account
+    fun getRelationShips(ids: List<Long>): List<Relationship>
 }
 
 class DefaultAccountRepository @Inject constructor(
@@ -15,5 +17,9 @@ class DefaultAccountRepository @Inject constructor(
 ) : AccountRepository{
     override fun getAccount(id: Long): Account {
         return Accounts(client).getAccount(id).execute()
+    }
+
+    override fun getRelationShips(ids: List<Long>): List<Relationship> {
+        return Accounts(client).getRelationships(ids).execute()
     }
 }
