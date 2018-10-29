3355981a3dc4fcdb88932978f375f22c97976e6f
yuzumone <yuzumone@outlook.com>
2018-10-29 19:10:39 +0900

    Update account repository

--- app/src/main/java/net/yuzumone/tootrus/data/mastodon/AccountRepository.kt
+++ app/src/main/java/net/yuzumone/tootrus/data/mastodon/AccountRepository.kt
@@ -1,8 +1,10 @@
 package net.yuzumone.tootrus.data.mastodon
 
 import com.sys1yagi.mastodon4j.MastodonClient
+import com.sys1yagi.mastodon4j.api.Range
 import com.sys1yagi.mastodon4j.api.entity.Account
 import com.sys1yagi.mastodon4j.api.entity.Relationship
+import com.sys1yagi.mastodon4j.api.entity.Status
 import com.sys1yagi.mastodon4j.api.method.Accounts
 import javax.inject.Inject
 import javax.inject.Named
@@ -10,6 +12,13 @@ import javax.inject.Named
 interface AccountRepository {
     fun getAccount(id: Long): Account
     fun getRelationShips(ids: List<Long>): List<Relationship>
+    fun getStatuses(id: Long,
+                    onlyMedia: Boolean,
+                    excludeReplies: Boolean,
+                    pinned: Boolean,
+                    range: Range): List<Status>
+    fun getFollowers(id: Long, range: Range): List<Account>
+    fun getFollowing(id: Long, range: Range): List<Account>
 }
 
 class DefaultAccountRepository @Inject constructor(
@@ -21,5 +30,21 @@ class DefaultAccountRepository @Inject constructor(
 
     override fun getRelationShips(ids: List<Long>): List<Relationship> {
         return Accounts(client).getRelationships(ids).execute()
+    }
+
+    override fun getStatuses(id: Long,
+                             onlyMedia: Boolean,
+                             excludeReplies: Boolean,
+                             pinned: Boolean,
+                             range: Range): List<Status> {
+        return Accounts(client).getStatuses(id, onlyMedia, excludeReplies, pinned, range).execute().part
+    }
+
+    override fun getFollowers(id: Long, range: Range): List<Account> {
+        return Accounts(client).getFollowers(id, range).execute().part
+    }
+
+    override fun getFollowing(id: Long, range: Range): List<Account> {
+        return Accounts(client).getFollowing(id, range).execute().part
     }
 }
