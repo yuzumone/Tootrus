444d07f156060874477c745293ea3ad01b896b29
yuzumone <yuzumone@outlook.com>
2018-10-29 19:10:55 +0900

    Add usecase

---
+++ app/src/main/java/net/yuzumone/tootrus/domain/mastodon/account/GetFollowersUseCase.kt
@@ -0,0 +1,14 @@
+package net.yuzumone.tootrus.domain.mastodon.account
+
+import com.sys1yagi.mastodon4j.api.Range
+import com.sys1yagi.mastodon4j.api.entity.Account
+import net.yuzumone.tootrus.data.mastodon.AccountRepository
+import net.yuzumone.tootrus.domain.UseCase
+import javax.inject.Inject
+
+class GetFollowersUseCase @Inject constructor(
+        private val repository: AccountRepository
+) : UseCase<Pair<Long, Range>, List<Account>>() {
+    override suspend fun run(params: Pair<Long, Range>) =
+            repository.getFollowers(params.first, params.second)
+}
