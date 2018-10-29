1a983d366742e538b2744623df6b296db5f099db
yuzumone <yuzumone@outlook.com>
2018-10-29 19:16:40 +0900

    Update ProfileViewModel

--- app/src/main/java/net/yuzumone/tootrus/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/net/yuzumone/tootrus/ui/profile/ProfileViewModel.kt
@@ -2,19 +2,27 @@ package net.yuzumone.tootrus.ui.profile
 
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import com.sys1yagi.mastodon4j.api.Range
 import com.sys1yagi.mastodon4j.api.entity.Account
 import com.sys1yagi.mastodon4j.api.entity.Relationship
+import com.sys1yagi.mastodon4j.api.entity.Status
 import net.yuzumone.tootrus.domain.Failure
 import net.yuzumone.tootrus.domain.Success
-import net.yuzumone.tootrus.domain.mastodon.account.GetAccountAndRelationShipUseCase
+import net.yuzumone.tootrus.domain.mastodon.account.*
 import javax.inject.Inject
 
 class ProfileViewModel @Inject constructor(
-        private val getAccountAndRelationShipsUseCase: GetAccountAndRelationShipUseCase
+        private val getAccountAndRelationShipsUseCase: GetAccountAndRelationShipUseCase,
+        private val getStatusesUseCase: GetStatusesUseCase,
+        private val getFollowingUseCase: GetFollowingUseCase,
+        private val getFollowersUseCase: GetFollowersUseCase
 ) : ViewModel() {
 
     val account = MutableLiveData<Account>()
     val relationship = MutableLiveData<Relationship>()
+    val statuses = MutableLiveData<List<Status>>()
+    val followings = MutableLiveData<List<Account>>()
+    val followers = MutableLiveData<List<Account>>()
     val error = MutableLiveData<Exception>()
 
     fun getAccountAndRelationShip(id: Long) {
@@ -24,6 +32,40 @@ class ProfileViewModel @Inject constructor(
                     account.value = it.value.first
                     relationship.value = it.value.second
                 }
+                is Failure -> error.value = it.reason
+            }
+        }
+    }
+
+    fun getStatuses(
+            id: Long,
+            onlyMedia: Boolean = false,
+            excludeReplies: Boolean = false,
+            pinned: Boolean = false,
+            range: Range = Range()
+    ) {
+        val params = Params(id, onlyMedia, excludeReplies, pinned, range)
+        getStatusesUseCase(params) {
+            when (it) {
+                is Success -> statuses.value = it.value
+                is Failure -> error.value = it.reason
+            }
+        }
+    }
+
+    fun getFollowing(id: Long, range: Range = Range()) {
+        getFollowingUseCase(Pair(id, range)) {
+            when (it) {
+                is Success -> followings.value = it.value
+                is Failure -> error.value = it.reason
+            }
+        }
+    }
+
+    fun getFollowers(id: Long, range: Range = Range()) {
+        getFollowersUseCase(Pair(id, range)) {
+            when (it) {
+                is Success -> followers.value = it.value
                 is Failure -> error.value = it.reason
             }
         }
