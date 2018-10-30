304143b643f27271f80820e82e5176015a4e8adf
yuzumone <yuzumone@outlook.com>
2018-10-30 17:59:47 +0900

    Add usecase

---
+++ app/src/main/java/net/yuzumone/tootrus/domain/mastodon/account/PostFollowUseCase.kt
@@ -0,0 +1,12 @@
+package net.yuzumone.tootrus.domain.mastodon.account
+
+import com.sys1yagi.mastodon4j.api.entity.Relationship
+import net.yuzumone.tootrus.data.mastodon.AccountRepository
+import net.yuzumone.tootrus.domain.UseCase
+import javax.inject.Inject
+
+class PostFollowUseCase @Inject constructor(
+        private val repository: AccountRepository
+) : UseCase<Long, Relationship>() {
+    override suspend fun run(params: Long) = repository.postFollow(params)
+}
