105134e816b9c16529292428b79314338dbf37c6
yuzumone <yuzumone@outlook.com>
2018-11-04 16:22:26 +0900

    Add image preview
    close #59

--- app/src/main/java/net/yuzumone/tootrus/ui/common/ThumbnailView.kt
+++ app/src/main/java/net/yuzumone/tootrus/ui/common/ThumbnailView.kt
@@ -4,10 +4,12 @@ import android.content.Context
 import android.databinding.DataBindingUtil
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.LinearLayout
 import com.sys1yagi.mastodon4j.api.entity.Attachment
 import net.yuzumone.tootrus.R
 import net.yuzumone.tootrus.databinding.ViewThumbnailBinding
+import net.yuzumone.tootrus.ui.MediaPreviewActivity
 
 class ThumbnailView : LinearLayout {
 
@@ -26,8 +28,14 @@ class ThumbnailView : LinearLayout {
     }
 
     fun setAttachments(attachments: List<Attachment>) {
-        attachments.forEach {
-            bindView(it)
+        attachments.forEachIndexed { index, attachment ->
+            val v = bindView(attachment)
+            v.setOnClickListener { _ ->
+                val media = MediaPreviewActivity.Media(attachments, index)
+                val intent = MediaPreviewActivity.createIntent(context, media)
+                context.startActivity(intent)
+            }
+            addView(v)
         }
     }
 
@@ -35,12 +43,12 @@ class ThumbnailView : LinearLayout {
         removeAllViews()
     }
 
-    private fun bindView(attachment: Attachment) {
+    private fun bindView(attachment: Attachment): View {
         val binding = DataBindingUtil.inflate<ViewThumbnailBinding>(
                 LayoutInflater.from(context), R.layout.view_thumbnail, this, false
         ).also {
             it.attachment = attachment
         }
-        addView(binding.root)
+        return binding.root
     }
 }
---
+++ app/src/main/java/net/yuzumone/tootrus/ui/preview/ImagePreviewFragment.kt
@@ -0,0 +1,36 @@
+package net.yuzumone.tootrus.ui.preview
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.gson.Gson
+import com.sys1yagi.mastodon4j.api.entity.Attachment
+import net.yuzumone.tootrus.databinding.FragmentImagePreviewBinding
+
+class ImagePreviewFragment : Fragment() {
+
+    private lateinit var binding: FragmentImagePreviewBinding
+    private val attachment by lazy {
+        Gson().fromJson(arguments!!.getString(ARG_ATTACHMENT), Attachment::class.java)
+    }
+
+    companion object {
+        private const val ARG_ATTACHMENT = "attachment"
+        fun newInstance(attachment: Attachment): ImagePreviewFragment {
+            return ImagePreviewFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_ATTACHMENT, Gson().toJson(attachment))
+                }
+            }
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        binding = FragmentImagePreviewBinding.inflate(inflater, container, false)
+        binding.attachment = attachment
+        return binding.root
+    }
+}
