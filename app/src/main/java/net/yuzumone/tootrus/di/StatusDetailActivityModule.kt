a6946c2257d8f07e1fcb3bdb051f1426ca804733
yuzumone <yuzumone@outlook.com>
2018-11-13 19:46:43 +0900

    Fix status detail
    close #68

--- app/src/main/java/net/yuzumone/tootrus/di/StatusDetailActivityModule.kt
+++ app/src/main/java/net/yuzumone/tootrus/di/StatusDetailActivityModule.kt
@@ -3,16 +3,11 @@ package net.yuzumone.tootrus.di
 import android.arch.lifecycle.ViewModel
 import dagger.Binds
 import dagger.Module
-import dagger.android.ContributesAndroidInjector
 import dagger.multibindings.IntoMap
-import net.yuzumone.tootrus.ui.detail.StatusDetailFragment
 import net.yuzumone.tootrus.ui.detail.StatusDetailViewModel
 
 @Module
 abstract class StatusDetailActivityModule {
-
-    @ContributesAndroidInjector
-    abstract fun contributesStatusDetailFragment(): StatusDetailFragment
 
     @Binds
     @IntoMap
--- app/src/main/java/net/yuzumone/tootrus/ui/StatusDetailActivity.kt
+++ app/src/main/java/net/yuzumone/tootrus/ui/StatusDetailActivity.kt
@@ -1,25 +1,28 @@
 package net.yuzumone.tootrus.ui
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
 import android.content.Context
 import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
 import dagger.android.AndroidInjection
-import dagger.android.AndroidInjector
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.support.HasSupportFragmentInjector
 import net.yuzumone.tootrus.R
 import net.yuzumone.tootrus.databinding.ActivityStatusDetailBinding
 import net.yuzumone.tootrus.ui.detail.StatusDetailFragment
+import net.yuzumone.tootrus.ui.detail.StatusDetailViewModel
 import javax.inject.Inject
 
-class StatusDetailActivity : AppCompatActivity(), HasSupportFragmentInjector {
+class StatusDetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityStatusDetailBinding
+    private lateinit var statusDetailViewModel: StatusDetailViewModel
     private val id: Long by lazy { intent.getLongExtra(ARG_ID, 0L) }
-    @Inject lateinit var dispatchingActivityInjector: DispatchingAndroidInjector<Fragment>
+    @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
 
     companion object {
         private const val ARG_ID = "id"
@@ -35,13 +38,20 @@ class StatusDetailActivity : AppCompatActivity(), HasSupportFragmentInjector {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_status_detail)
         setSupportActionBar(binding.toolbar)
+        statusDetailViewModel = ViewModelProviders.of(this, viewModelFactory)
+                .get(StatusDetailViewModel::class.java)
+        statusDetailViewModel.status.observe(this, Observer { status ->
+            binding.progress.visibility = View.GONE
+            if (status != null) {
+                val fragment = StatusDetailFragment.newInstance(status)
+                supportFragmentManager.beginTransaction().add(R.id.content, fragment).commit()
+            } else {
+                Toast.makeText(this, R.string.error, Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        })
         if (savedInstanceState == null) {
-            val fragment = StatusDetailFragment.newInstance(id)
-            supportFragmentManager.beginTransaction().add(R.id.content, fragment).commit()
+            statusDetailViewModel.getStatus(id)
         }
-    }
-
-    override fun supportFragmentInjector(): AndroidInjector<Fragment> {
-        return dispatchingActivityInjector
     }
 }
