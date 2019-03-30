2c7d62fc97ec2c0ce9146af44f74473063000cfc
yuzumone <yuzumone@outlook.com>
2019-03-30 17:16:16 +0900

    Update PostStatusDialogFragment

--- app/src/main/java/net/yuzumone/tootrus/ui/poststatus/PostStatusDialogFragment.kt
+++ app/src/main/java/net/yuzumone/tootrus/ui/poststatus/PostStatusDialogFragment.kt
@@ -2,12 +2,16 @@ package net.yuzumone.tootrus.ui.poststatus
 
 import android.app.Dialog
 import android.content.DialogInterface
-import androidx.databinding.DataBindingUtil
 import android.os.Bundle
-import androidx.fragment.app.DialogFragment
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.sys1yagi.mastodon4j.api.entity.Status
 import net.yuzumone.tootrus.R
 import net.yuzumone.tootrus.databinding.FragmentPostStatusBinding
@@ -16,7 +20,9 @@ import net.yuzumone.tootrus.service.PostStatusService
 
 class PostStatusDialogFragment : DialogFragment() {
 
+    private var visibility: Status.Visibility = Status.Visibility.Public
     private lateinit var binding: FragmentPostStatusBinding
+    private lateinit var viewModel: PostStatusDialogViewModel
     private val inReplyToAcct: String? by lazy { arguments?.getString(ARG_IN_REPLY_TO_ACCT) }
     private val inReplyToId: Long? by lazy { arguments?.getLong(ARG_IN_REPLY_TO_ID) }
 
@@ -34,9 +40,11 @@ class PostStatusDialogFragment : DialogFragment() {
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        viewModel = ViewModelProviders.of(this).get(PostStatusDialogViewModel::class.java)
         val dialog = Dialog(activity!!)
         binding = DataBindingUtil.inflate(LayoutInflater.from(activity),
                 R.layout.fragment_post_status, null, false)
+        binding.viewModel = viewModel
         dialog.setContentView(binding.root)
         dialog.window?.apply {
             setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
@@ -48,6 +56,14 @@ class PostStatusDialogFragment : DialogFragment() {
             binding.inputText.setText(s)
             binding.inputText.setSelection(s.length)
         }
+
+        viewModel.visibility.observe(this, Observer {
+            visibility = it
+            updateVisibilityMenu()
+            if (binding.viewVisibility.visibility == View.VISIBLE) {
+                binding.viewVisibility.visibility = View.GONE
+            }
+        })
         return dialog
     }
 
@@ -57,15 +73,47 @@ class PostStatusDialogFragment : DialogFragment() {
             dismiss()
         }
         binding.toolbar.inflateMenu(R.menu.menu_post_status)
+        binding.toolbar.inflateMenu(R.menu.menu_visibility_public)
         binding.toolbar.setOnMenuItemClickListener {
             when (it.itemId) {
                 R.id.menu_post_status -> {
                     val text = binding.inputText.text.toString()
-                    postStatus(text, inReplyToId, null, false, null)
+                    postStatus(text, inReplyToId, null, false, null, visibility)
                     dismiss()
+                }
+                R.id.menu_visibility_public, R.id.menu_visibility_unlisted,
+                R.id.menu_visibility_private, R.id.menu_visibility_direct -> {
+                    val anim = AnimationUtils.loadAnimation(activity, R.anim.anim_view_quick)
+                    if (binding.viewVisibility.visibility == View.GONE) {
+                        binding.viewVisibility.visibility = View.VISIBLE
+                        binding.viewVisibility.startAnimation(anim)
+                    } else {
+                        binding.viewVisibility.visibility = View.GONE
+                    }
                 }
             }
             false
+        }
+    }
+
+    private fun updateVisibilityMenu() {
+        binding.toolbar.menu.removeItem(R.id.menu_visibility_public)
+        binding.toolbar.menu.removeItem(R.id.menu_visibility_unlisted)
+        binding.toolbar.menu.removeItem(R.id.menu_visibility_private)
+        binding.toolbar.menu.removeItem(R.id.menu_visibility_direct)
+        when (visibility) {
+            Status.Visibility.Public -> {
+                binding.toolbar.inflateMenu(R.menu.menu_visibility_public)
+            }
+            Status.Visibility.Unlisted -> {
+                binding.toolbar.inflateMenu(R.menu.menu_visibility_unlisted)
+            }
+            Status.Visibility.Private -> {
+                binding.toolbar.inflateMenu(R.menu.menu_visibility_private)
+            }
+            Status.Visibility.Direct -> {
+                binding.toolbar.inflateMenu(R.menu.menu_visibility_direct)
+            }
         }
     }
 
