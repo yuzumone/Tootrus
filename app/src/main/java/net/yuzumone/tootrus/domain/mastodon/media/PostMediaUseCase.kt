15597a7420e317b90a70523e38f5fee9027d97be
yuzumone <yuzumone@outlook.com>
2019-05-25 18:34:57 +0900

    Add PostMediaUseCase

---
+++ app/src/main/java/net/yuzumone/tootrus/domain/mastodon/media/PostMediaUseCase.kt
@@ -0,0 +1,14 @@
+package net.yuzumone.tootrus.domain.mastodon.media
+
+import com.sys1yagi.mastodon4j.api.entity.Attachment
+import net.yuzumone.tootrus.data.mastodon.MediaRepository
+import net.yuzumone.tootrus.domain.UseCase
+import okhttp3.MultipartBody
+import javax.inject.Inject
+
+class PostMediaUseCase @Inject constructor(
+        private val repository: MediaRepository
+) : UseCase<List<MultipartBody.Part> , List<Attachment>>() {
+    override suspend fun run(params: List<MultipartBody.Part>) =
+            repository.postMedia(params)
+}
