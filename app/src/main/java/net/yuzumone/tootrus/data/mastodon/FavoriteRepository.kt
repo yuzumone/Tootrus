06a79ad1ebf2ee8bae77de9995b01d22e7145ffb
yuzumone <yuzumone@outlook.com>
2019-06-20 22:33:28 +0900

    Add GetFragmentUseCase

---
+++ app/src/main/java/net/yuzumone/tootrus/data/mastodon/FavoriteRepository.kt
@@ -0,0 +1,19 @@
+package net.yuzumone.tootrus.data.mastodon
+
+import com.sys1yagi.mastodon4j.MastodonClient
+import com.sys1yagi.mastodon4j.api.Range
+import com.sys1yagi.mastodon4j.api.entity.Status
+import com.sys1yagi.mastodon4j.api.method.Favourites
+import javax.inject.Inject
+import javax.inject.Named
+
+interface FavoriteRepository {
+    fun getFavorites(range: Range): List<Status>
+}
+
+class DefaultFavoriteRepository @Inject constructor(
+        @Named("client") private val client: MastodonClient
+): FavoriteRepository {
+    override fun getFavorites(range: Range): List<Status> =
+            Favourites(client).getFavourites(range).execute().part
+}
